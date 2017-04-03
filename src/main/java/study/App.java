72f2e862989640d890c5a7678e06e44c65d7cba0
andrew.jung <andrew.jung@kakaocorp.com>
2017-04-03 15:34:46 +0900

    excel to pdf

--- src/main/java/study/App.java
+++ src/main/java/study/App.java
@@ -1,12 +1,60 @@
 package study;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
 
 public class App {
+    // copy from : http://thinktibits.blogspot.kr/2012/12/POI-iText-Convert-XLS-to-PDF-Java-Program.html
+    private static void saveToPDF(String excel, String pdf) throws IOException, DocumentException {
+        BaseFont bfKorean = BaseFont.createFont("HYGoThic-Medium", "UniKS-UCS2-H", BaseFont.NOT_EMBEDDED);
+        Font fontKorean = new Font(bfKorean, 10, Font.NORMAL);
+
+        FileInputStream input_document = new FileInputStream(new File(excel));
+        HSSFWorkbook my_xls_workbook = new HSSFWorkbook(input_document);
+        HSSFSheet my_worksheet = my_xls_workbook.getSheetAt(0);
+        Iterator<Row> rowIterator = my_worksheet.iterator();
+        Document iText_xls_2_pdf = new Document();
+        PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream(pdf));
+        iText_xls_2_pdf.open();
+        PdfPTable my_table = new PdfPTable(my_worksheet.getRow(0).getLastCellNum());
+        PdfPCell table_cell;
+
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next(); //Fetch CELL
+                switch (cell.getCellType()) { //Identify CELL type
+                    case Cell.CELL_TYPE_STRING:
+                        table_cell = new PdfPCell(new Phrase(cell.getStringCellValue(), fontKorean));
+                        my_table.addCell(table_cell);
+                        break;
+                }
+            }
+
+        }
+
+        iText_xls_2_pdf.add(my_table);
+        iText_xls_2_pdf.close();
+        input_document.close(); //close xls
+    }
+
     private static String getCellValue(CellValue cellValue) {
         String value = "";
 
@@ -56,6 +104,8 @@ public class App {
         fileOut.close();
 
         System.out.println(String.format("전체 %d 행을 저장하였습니다", rowIdx));
+
+        saveToPDF("merged.xlsx", "merged.pdf");
     }
 
     private static int sheetToMergedSheet(Workbook wb, Sheet newSheet, int rowIdx, FormulaEvaluator evaluator) {
